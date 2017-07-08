0feef84850540a43f489cc737aa86e609026fbb1
Brian Kerr <brian_kerr@unifirst.com>
2017-07-08 13:29:48 -0400

    Added interval refresh of every one minute - filter persists after refresh

--- Client/src/app/twitter/twitter-timeline/twitter-timeline.component.ts
+++ Client/src/app/twitter/twitter-timeline/twitter-timeline.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params }   from '@angular/router';
+import { Observable } from 'rxjs/Rx';
+import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/takeUntil';
 
 import { TwitterService } from '../shared/twitter.service';
 import { Tweet } from '../shared/tweet.model';
@@ -10,29 +13,47 @@ import { Tweet } from '../shared/tweet.model';
   templateUrl: './twitter-timeline.component.html',
   styleUrls: ['./twitter-timeline.component.css']
 })
-export class TwitterTimelineComponent implements OnInit {
+export class TwitterTimelineComponent implements OnInit, OnDestroy {
+  private ngUnsubscribe: Subject<void> = new Subject<void>();
+
   tweets: Tweet[];
   filteredTweets: Tweet[];
+  searchTerm: string;
 
   constructor(private twitterService: TwitterService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.route.params
-    .switchMap((params: Params) => this.twitterService.getRecentTweets(params['id']))
-    .subscribe(tweets => {
-      this.tweets = tweets;
-      this.filteredTweets = tweets;
-      }
-    );
+    this.updateTweets()
+    
+    Observable
+      .interval(60000)
+      .takeUntil(this.ngUnsubscribe)
+      .subscribe(x => {
+        this.updateTweets();
+      });
   }
 
-  onKey(event: any) { // without type info
-    let searchTerm = event.target.value
-       
+  updateTweets() {
+    this.route.params
+        .switchMap((params: Params) => this.twitterService.getRecentTweets(params['id']))
+        .subscribe(tweets => {
+          this.tweets = tweets;
+          this.filterTweetsOn(this.searchTerm);
+          }
+        );
+  }
+
+  filterTweetsOn(searchTerm: string) {      
+    this.searchTerm = searchTerm;
     this.filteredTweets = [];
 
     this.tweets.forEach(tweet => {
-      if (tweet['Text'].search(new RegExp(searchTerm, "i")) >= 0) this.filteredTweets.push(tweet);
+      if (tweet['Text'].search(new RegExp(this.searchTerm, "i")) >= 0) this.filteredTweets.push(tweet);
     });
   }
+
+  ngOnDestroy() {
+        this.ngUnsubscribe.next();
+        this.ngUnsubscribe.complete();
+    }
 }
